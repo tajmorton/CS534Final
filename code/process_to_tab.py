551c58624107146f9858270e292dab01ebd664fc
Taj Morton <mortont@onid.orst.edu>
2013-05-25 18:53:37 -0700

    Initial import of CS534 Final Project code (for converting original ad dataset into Orange TAB format).

---
+++ code/process_to_tab.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import sys
+from collections import OrderedDict
+
+def parse_names(names_filename):
+    classes = []
+    feature_list = OrderedDict()
+
+    for line in open(names_filename, 'r'):
+        line = line.split('|', 1)[0]
+        if len(line.strip()) == 0:
+            continue
+
+        if line.strip()[-1] != '.':
+            classes = map(lambda x: x.strip(), line.split(','))
+            continue
+
+        # a feature line
+        name, f_type = line.split(': ')
+        if f_type.startswith("continuous"):
+            feature_list[name] = "continuous"
+        else: # everything is is discrete (binary) in this data
+            feature_list[name] = "discrete"
+
+    return feature_list, classes
+
+def write_tab_header(tab_out, feature_list, class_name):
+    for f in feature_list.keys(): 
+        tab_out.write("%s\t" % f)
+
+    tab_out.write("%s\n" % class_name)
+
+    for v in feature_list.values():
+        tab_out.write("%s\t" % v)
+
+    tab_out.write("discrete\n") # class if discrete
+
+    tab_out.write("\t"*len(feature_list))
+    tab_out.write("class\n")
+
+def write_data_tab(tab_out, datafile):
+    for line in open(datafile):
+        tab_out.write(line.strip()[:-1].replace(",","\t"))
+        tab_out.write("\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print >> sys.stderr, "Usage: process_to_tab.py names data tab_out"
+        sys.exit(1)
+
+    features, classes = parse_names(sys.argv[1])
+
+    with open(sys.argv[3], 'w') as tab_out:
+        write_tab_header(tab_out, features, "image_class")
+        write_data_tab(tab_out, sys.argv[2])
