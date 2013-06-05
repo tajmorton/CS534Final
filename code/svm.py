2afc2d5e2af4678024421246f35372fb25c9df74
Austin Sharp <austinsharp111@gmail.com>
2013-06-04 23:50:05 -0700

    added svm, linear svm does well
    other svm learners don't work at all (they end up decision stump style)

---
+++ code/svm.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+## SVM Classifier
+import sys
+import Orange, orange, orngTest, orngStat
+import proj_utils
+import os
+
+"""
+Trains a Linear SVM classifier on the given dataset.
+"""
+def train_classifier(data):
+    learner = Orange.classification.svm.LinearSVMLearner()
+    classifier = learner(data)
+    return classifier
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print >> sys.stderr, "Usage: naive_bayes.py [TAB_FILE]"
+        sys.exit(1)
+
+    data = proj_utils.load_data(sys.argv[1])
+
+    train_data, test_data = proj_utils.partition_data(data)
+
+    model = train_classifier(train_data)
+    train_CA, train_results = proj_utils.test_classifier(model, train_data)
+    test_CA, test_results = proj_utils.test_classifier(model, test_data)
+
+    #print "Train Accuracy: %f, Test Accuracy: %f" % (train_CA, test_CA)
+    train_stats = proj_utils.get_stats(train_results)
+    test_stats = proj_utils.get_stats(test_results)
+
+    print "Train:\n%s" % str(train_stats)
+    print "\nTest:\n%s" % str(test_stats)
+
+    f = open(os.path.dirname(__file__) + '\\lsvmResults.txt', 'w+')
+    f.write("Train:\n")
+    f.write(str(train_stats) + "\n")
+    f.write("Test:\n")
+    f.write(str(test_stats))
+    f.close()
