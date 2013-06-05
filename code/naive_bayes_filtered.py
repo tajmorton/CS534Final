3a1948e57aea41befccc039ad95b0ff9d57f2e8c
Austin Sharp <austinsharp111@gmail.com>
2013-06-04 23:29:39 -0700

    filter number as arg, output file has features

--- code/naive_bayes_filtered.py
+++ code/naive_bayes_filtered.py
@@ -15,16 +15,15 @@ def train_classifier(data, features):
 	return nb_classifier
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage: naive_bayes.py [TAB_FILE]"
+    if len(sys.argv) != 3:
+        print >> sys.stderr, "Usage: naive_bayes_filtered.py [TAB_FILE] [NUM_FEATURES]"
         sys.exit(1)
 
     data = proj_utils.load_data(sys.argv[1])
-
+    features = int(sys.argv[2])
 	
-    features = 200
     train_data, test_data = proj_utils.partition_data(data)
-
+	
     model = train_classifier(train_data, features)
     train_CA, train_results = proj_utils.test_classifier(model, train_data)
     test_CA, test_results = proj_utils.test_classifier(model, test_data)
@@ -35,11 +34,14 @@ if __name__ == "__main__":
 
     print "Train:\n%s" % str(train_stats)
     print "\nTest:\n%s" % str(test_stats)
+    print "\nFeatures:\n%s" % str(model.domain)
 
     f = open(os.path.dirname(__file__) + '\\naiveBayesFilteredResults' + str(features) + '.txt', 'w+')
     f.write("Train:\n")
     f.write(str(train_stats) + "\n")
     f.write("Test:\n")
     f.write(str(test_stats))
+    f.write("\nFeatures Chosen:\n")
+    f.write(str(model.domain))
     f.close()
 
