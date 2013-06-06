84cbea31b68574a9db188581621135287b6b41c1
mendesm@onid.orst.edu <mendesm@onid.orst.edu>
2013-06-05 23:34:15 -0700

    Fixed logistic regression w cross validation

--- code/logistic_regression_filtered_cv.py
+++ code/logistic_regression_filtered_cv.py
@@ -28,19 +28,24 @@ Trains Logistic Regression with selected features
 """
 def train_classifier(data, features):
     logr = Orange.classification.logreg.LogRegLearner()
-    learner = Orange.feature.selection.FilteredLearner(logr, filter=Orange.feature.selection.FilterBestN(n=features), name='filtered')
-    logr_classifier = learner(data)
+    #learner = Orange.feature.selection.FilteredLearner(logr, filter=Orange.feature.selection.FilterBestN(n=features), name='filtered')
+    logr_classifier = logr(data)
     return logr_classifier
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage: logistic_regression_filtered_cv.py [TAB_FILE]"
+    if len(sys.argv) != 4:
+        print >> sys.stderr, "Usage: logistic_regression_filtered_cv.py [TAB_FILE] [NUM_FOLDS] [NUM_FEATURES]"
         sys.exit(1)
 
-    data = proj_utils.load_data(sys.argv[1])
+    start_data = proj_utils.load_data(sys.argv[1])
 
-    cv_folds = 10
-    features = 5
+    cv_folds = int(sys.argv[2])
+    features = int(sys.argv[3])
+    
+    # default scoring algorithm
+    scores = Orange.feature.scoring.score_all(start_data)
+    data = Orange.feature.selection.select(start_data, scores, features)
+    
     train_data, test_data = proj_utils.partition_data(data)
     
     selection = orange.MakeRandomIndicesCV(data, cv_folds)
@@ -79,8 +84,8 @@ if __name__ == "__main__":
         spe2 += test_stats.get('Specificity')
         acc2 += test_stats.get('Accuracy')       
         
-        print "Training set %d: %s %s %s\n" % (test_fold+1, sen1, spe1, acc1)
-        print "Test set %d: %s %s %s\n" % (test_fold+1, sen1, spe1, acc1)
+        print "Training set %d: %s %s %s\n" % (test_fold+1, sen1/(test_fold+1), spe1/(test_fold+1), acc1/(test_fold+1))
+        print "Test set %d: %s %s %s\n" % (test_fold+1, sen2/(test_fold+1), spe2/(test_fold+1), acc2/(test_fold+1))
     acc1 /= cv_folds
     sen1 /= cv_folds
     spe1 /= cv_folds
@@ -94,10 +99,10 @@ if __name__ == "__main__":
     #print "Train Accuracy: %f, Test Accuracy: %f" % (train_CA, test_CA)
 
 
-    print "Train:\n Sensitivity: %s, Specificity: %s, Accuracy: %s" % (str(sen1), str(spe1), str(acc1))
-    print "Test:\n Sensitivity: %s, Specificity: %s, Accuracy: %s" % (str(sen2), str(spe2), str(acc2))
+    print "Train:\n Sensitivity: %s, Specificity: %s, Accuracy: %s\n" % (str(sen1), str(spe1), str(acc1))
+    print "Test:\n Sensitivity: %s, Specificity: %s, Accuracy: %s\n" % (str(sen2), str(spe2), str(acc2))
     
     f = open(os.path.dirname(__file__) + '\\logisticRegressionFilteredCVResults' + str(features) + '.txt', 'w+')
-    f.write("Train:\n Sensitivity: %s, Specificity: %s, Accuracy: %s" % (str(sen1), str(spe1), str(acc1)))
-    f.write("Test:\n Sensitivity: %s, Specificity: %s, Accuracy: %s" % (str(sen2), str(spe2), str(acc2)))   
+    f.write("Train:\n Sensitivity: %s, Specificity: %s, Accuracy: %s\n" % (str(sen1), str(spe1), str(acc1)))
+    f.write("Test:\n Sensitivity: %s, Specificity: %s, Accuracy: %s\n" % (str(sen2), str(spe2), str(acc2)))   
     f.close()
