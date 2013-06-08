62b688b40ac841ecbf56ca3abc16a981ae9ebe3f
Taj Morton <mortont@onid.orst.edu>
2013-06-08 16:14:37 -0700

    Test logistic regression on ranges of top scoring features and record performance.

--- code/svd.R
+++ code/svd.R
@@ -20,6 +20,21 @@ test_model <- function(model, features, classes, testing_indices, s = NULL) {
     return(confusionMatrix(data=pred_factors, classes[testing_indices], positive=levels(classes[testing_indices])[-1]))
 }
 
+train_models_with_feature_range <- function(pc_ranges, ordered_feature_names, features, classes, training_indices, nfolds = 10, family="binomial") {
+    results_table <- data.frame()
+    for (pcs in pc_ranges) {
+        print(pcs)
+        model <- train_model(features[,ordered_feature_names[1:pcs], drop=FALSE], classes, training_indices, nfolds, family)
+        model_results <- test_model(model, features[,ordered_feature_names[1:pcs]], classes, -training_indices, s=model$lambda.min)
+
+        results_table <- rbind(results_table, c(model_results$byClass, model_results$overall["Accuracy"]))
+        colnames(results_table) <- c(names(model_results$byClass), "Accuracy")
+    }
+
+    row.names(results_table) <- pc_ranges
+    return(results_table)
+}
+
 train_models_with_pc_range <- function(pc_ranges, features, classes, training_indices, nfolds = 10, family="binomial") {
     results_table <- data.frame()
     for (pcs in pc_ranges) {
